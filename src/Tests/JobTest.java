224d5cc9d4fcf8cd705ecbb22ca3b6ff001ac354
tarabyte84 <tekotthoff@me.com>
2020-12-19 19:36:05 -0600

    Created Tests package and JobTest class, imported Junit library.

---
+++ src/Tests/JobTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.junit.Test;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+
+public class JobTest {
+
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
+
+    @Test
+    public void
+
+}
