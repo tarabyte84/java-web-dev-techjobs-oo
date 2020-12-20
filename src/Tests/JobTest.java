89dc4fbb5cce0e72643c394c7519ab0173c9bb73
tarabyte84 <tekotthoff@me.com>
2020-12-19 20:20:18 -0600

    Wrote tests for empty constructor and to test that the full constructor sets all fields. All tests passing.

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -1,10 +1,29 @@
 package Tests;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
 import static junit.framework.TestCase.fail;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class JobTest {
+
+//    Car test_car;
+//
+//    @Before
+//    public void createCarObject() {
+//        test_car = new Car("Toyota", "Prius", 10, 50);
+//    }
+    Job test_job_1;
+    Job test_job_2;
+
+    @Before
+    public void createJobObjects() {
+        test_job_1 = new Job();
+        test_job_2 = new Job();
+    }
+
 
     @Test
     public void emptyTest() {
@@ -12,6 +31,29 @@ public class JobTest {
     }
 
     @Test
-    public void
+    public void testSettingJobId() { assertFalse(test_job_1.equals(test_job_2));}
 
+    @Test
+    public void job2ShouldBe1MoreThanJob1() { assertTrue(test_job_2.getId() -
+            test_job_1.getId() == 1);}
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new
+            PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals(job3.getName(), "Product tester");
+        assertEquals(job3.getEmployer().getValue(), "ACME");
+        assertEquals(job3.getLocation().getValue(), "Desert");
+        assertEquals(job3.getPositionType().getValue(), "Quality control");
+        assertEquals(job3.getCoreCompetency().getValue(), "Persistence");
+
+        assertTrue(job3 instanceof Job);
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertTrue(job3.getLocation() instanceof Location);
+        assertTrue(job3.getPositionType() instanceof PositionType);
+        assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+
+
+    }
 }
